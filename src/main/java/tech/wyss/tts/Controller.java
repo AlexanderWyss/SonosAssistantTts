151271777bf9112228b8efc71ee4d629c3890333
Alexander Wyss <alexsilvan.wyss@gmail.com>
2020-11-28 18:33:56 +0100

    file names

--- src/main/java/tech/wyss/tts/Controller.java
+++ src/main/java/tech/wyss/tts/Controller.java
@@ -8,18 +8,30 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.nio.file.Path;
 import java.nio.file.Paths;
 
 @RestController
 @RequestMapping("/api")
 public class Controller {
+    private static int counter = 0;
+    private static final int MAX_FILES = 5;
+
     @PostMapping("/tts")
     public TtsUri generate(@RequestBody TtsRequest body) {
+        String fileName = getNextFileName();
         SpeechConfig speechConfig = SpeechConfig.fromSubscription(System.getenv("SPEECH_KEY"), System.getenv("SPEECH_REGION"));
-        AudioConfig audioConfig = AudioConfig.fromWavFileOutput(Paths.get(System.getenv("AUDIO_PATH"),"test.wav").toString());
+        AudioConfig audioConfig = AudioConfig.fromWavFileOutput(Paths.get(System.getenv("AUDIO_PATH"), fileName).toString());
         speechConfig.setSpeechSynthesisLanguage("de-DE");
         SpeechSynthesizer synthesizer = new SpeechSynthesizer(speechConfig, audioConfig);
         synthesizer.SpeakText(body.getText());
-        return new TtsUri("https://sonos.wyss.tech/tts/test.wav");
+        return new TtsUri("https://sonos.wyss.tech/tts/" + fileName);
+    }
+
+    private String getNextFileName() {
+        if (counter == MAX_FILES) {
+            counter = 0;
+        }
+        return "tts-" + counter++ + ".wav";
     }
 }
